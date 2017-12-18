e27b43e80f0e3de7d730a7371a96050fab91365f
tokyo <tokyo@tk2-225-22092.vs.sakura.ne.jp>
2017-12-18 16:52:38 +0900

    processのコントローラーを一部加筆。

--- src/UI/FXMLTabPageProcessController.java
+++ src/UI/FXMLTabPageProcessController.java
@@ -5,14 +5,22 @@
  */
 package UI;
 
+import DB.DatabaseUty;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -38,12 +46,44 @@ public class FXMLTabPageProcessController implements Initializable {
     @FXML
     private Label subTitle;
 
+        private void initFocuseConditionForTask() { // 要改善最初に存在確認をしてから編集不可とすべき。
+        this.bookUUIDorHumanyCode.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0,
+                    Boolean oldPropertyValue, Boolean newPropertyValue) {
+                if (newPropertyValue) {
+                    //textField_SHIP_ID.setEditable(true);
+                    System.out.println("Textfield on focus");
+                } else {
+                    //textField_SHIP_ID.setEditable(false);
+                    bookUUIDorHumanyCode.setDisable(true); // 編集不可になっていることが明確。ただし文字は見にくい。
+                    System.out.println("Textfield out focus");
+                    /*
+                    ResultSet rs = DatabaseUty.getResultSetByKey("ship", "id", textField_SHIP_ID.getText().trim());
+                    try {
+                        rs.next();
+                        textField_SHIP_SERVICE.setText(rs.getString("SERVICE"));
+                        textArea_SHIP_NAME.setText(rs.getString("NAME"));
+                        textArea_SHIP_REMARK.setText(rs.getString("REMARK"));
+                        flgExsistShip = true;
+                    } catch (SQLException ex) {
+                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                        flgExsistShip = false;
+                        JOptionPane.showMessageDialog(null, "新規もしくはエラーです");
+                    }
+                    */
+                }
+            }
+        });
+    }
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        initFocuseConditionForTask(); // 主キーを保護する為にロック。
     }    
     
 }
